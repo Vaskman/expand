d4a123407086fac56c98c2effff6759d12efd6d6
Vaskman <z.vaskman@gmail.com>
2023-02-18 14:15:45 +0300

    expand

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args){
+import java.util.Arrays;
 
-    }
+public class Main {
+        public static void invert(int[] array) {
+            for (int i = 0; i < array.length / 2; i++) {
+                int temp = array[i];
+                array[i] = array[array.length - 1 - i];
+                array[array.length - 1 - i] = temp;
+            }
+            System.out.println(Arrays.toString(array));
+        }
+        public static void main(String[] args) {
+            int []arr={1,2,3};
+            invert(arr);
+        }
+
+
 }
